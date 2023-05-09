d172360ba7fb305cd9c071e3c80e5b1ff2723954
cyborf <dle3@oberlin.edu>
2023-05-09 19:33:38 -0400

    Progress with checkboxes

--- script.js
+++ script.js
@@ -4,7 +4,66 @@
 * Dan-Ha Le, Ananya Sayani, Zaina Saleem, Miriam Rairick
 */
 
+// Global variables:
+var myEssay = newArray(6);
+var error = false;
 
+// This function returns a list of sections and the time needed to complete them.
+function checkBoxSection(hours) {
+    document.getElementById("error").innerHTML = "Test"
+    let count = 0
+    // A list of variables correspond to the checkboxes
+    // var can be used globally, let is only scoped within this curly bracket
+
+    // Content of myEssay = [intro, litreview, case, analysis, methodology, conclusion]
+    var intro = document.getElementById("section2");
+    var litreview = document.getElementById("section3");
+    var cases = document.getElementById("section4");
+    var analysis = document.getElementById("section5");
+    var methodology = document.getElementById("section6");
+    var conclusion = document.getElementById("section7");
+    var abstract = document.getElementById("section1")
+
+    // Preliminarily check which sections have selected, returns an error if none.
+    if (intro.checked == true) {
+        myEssay[0] = 0;
+        count++;
+    }
+    if (litreview.checked == true) {
+        myEssay[1] = 0;
+        count++;
+    }
+    if (cases.checked == true) {
+        myEssay[2] = 0;
+        count++;
+    }
+    if (analysis.checked == true) {
+        myEssay[3] = 0;
+        count++;
+    }
+    if (methodology.checked== true) {
+        myEssay[4] = 0;
+        count++;
+    }
+    if (conclusion.checked== true) {
+        myEssay[5] = 0;
+        count++;
+    }
+    if (abstract.checked == true) {
+        myEssay[6] = 0;
+        count++;
+    }
+    else {
+        // returns an error if no sections are picked.
+
+        return error.innerHTML = "Please pick at least one checkbox!"
+    }
+    
+
+    // check for undefined: typeof array[index] == 'undefined'
+}
+
+// This function creates a schedule when the user clicks on the button
 function makeSchedule() {
     // Possibility: Maybe when the user click on the link we could have the results be displayed on a different HTML page?
 
@@ -16,19 +75,16 @@ function makeSchedule() {
     let paperDailyHours = document.querySelector("#paperDailyHours");
     let paperPace = document.querySelector("#paperPace");
 
-    // Algorithm here!
 
     let totalHours = paperDailyHours.value * paperDays.value;
     let totalHoursRequired = Math.round(paperLength.value / paperPace.value);
 
-    // Calculate the hours for each part of the essay:
+    // Prepare myEssay;
+    checkboxSection(totalHours);
+        // If there was an error, stop operations.
+    if (error == true) return;
 
-    let introHours = Math.round(totalHoursRequired / 5);
-    let bodyHours = Math.round(totalHoursRequired * 3 / 5);
-    let conclusionHours = totalHoursrequired - introHours - bodyHours;
 
-
-    // Assigning the hours (Turbo done):
 
     // Creating the array schedule:
     let schedule = new Array();
@@ -36,16 +92,6 @@ function makeSchedule() {
         schedule[i] = paperDailyHours.value;
     }
 
-    //Assigning the hours (hard):
-    /*
-     *
-     * Restrictions:
-     * - Conclusion must be done before last day
-     * - Introduction starts on the first day
-     * -
-     */
-
-    message.innerHTML = totalHoursRequired;
 
 
     // Output information here!
